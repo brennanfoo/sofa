31d93ee765f8b96eefbe992572252b41428e7aab
brennanfoo <brennan.foo@gmail.com>
2016-02-03 18:21:41 -0500

    Lots of changes

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :name, :url, :created_at, :updated_at
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(name:"Everything depends on your perspective",url:"http://i.imgur.com/KezAIUF.gif")
+Post.create(name:"Rice planting machine",url:"  http://i.imgur.com/YHoBqLR.gif")
+Post.create(name:"Harlem card trotters  ",url:" http://i.imgur.com/0zUSWJu.gif")
+Post.create(name:"Rock climber just shows his skills with a chair",url:" http://i.imgur.com/U5b2IDI.gif")
+Post.create(name:"Two guys docking mid air",url:"http://i.imgur.com/Q3vtiT9.gif")
+Post.create(name:"Clock that writes time",url:" http://i.imgur.com/31iPor3.gif")
+Post.create(name:"Camera in plastic bottle",url:"http://giant.gfycat.com/AnimatedGrippingCottonmouth.gif")
+Post.create(name:"Ketchup nanotech",url:"http://i.imgur.com/hQTaGDD.gif")
+Post.create(name:"Slow-motion slinky",url:" http://i.imgur.com/aGQpFgV.gif")
+Post.create(name:"Crushing a baseball",url:"  http://i.imgur.com/k0TJqUa.gif")
+Post.create(name:"Straight bar, curved hole",url:" http://i.imgur.com/Zj1rgLB.gif")
+Post.create(name:"1968 Stair Chair",url:"http://i.imgur.com/EJo1lO5.gif")
+Post.create(name:"Harvesting carrots",url:" http://i.imgur.com/AP4x35k.gif")
+Post.create(name:"Pregnancy",url:"http://i.imgur.com/cP9vlik.gif")
+Post.create(name:"Green screen",url:"http://i.imgur.com/MN3gNsK.jpg")
+Post.create(name:"Installing power lines",url:"http://i.imgur.com/F6AvFNv.jpg")
+Post.create(name:"Pool trick shot",url:"http://i.imgur.com/IaLF2FQ.gif")
+Post.create(name:"Mountain bike backflip",url:"http://i.imgur.com/NhXvk7O.gif")
+
